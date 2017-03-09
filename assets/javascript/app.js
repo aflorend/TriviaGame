2c653d8c39eaef5db625ac72484c70ce819e4e6b
Angelo <aflorend@gmail.com>
2017-03-09 16:31:43 -0600

    All questions and functionality completed. Now ready for styling.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,172 +3,172 @@ var triviaBank =
 [
 //***Q1***
 	{
-		questionText: "Test Q1",
+		questionText: "Perhaps best known for their stylized arrows and vibrant color esthetic, this studio continues to be praised for their work on the 1995 video game \"Wipeout.\"",
 
 		answers: 
 		[ 
 			{
-				text: "test1",
+				text: "Experimental Jetset",
+
+				correct: false
+			},
+
+			{
+				text: "The Designers Republic",
 
 				correct: true
 			},
 
 			{
-				text: "test2",
+				text: "MNML",
 
 				correct: false
 			},
 
 			{
-				text: "test3",
-
-				correct: false
-			},
-
-			{
-				text: "test4",
+				text: "Pentagram",
 
 				correct: false
 			}
 		],
 
-		response: "Response Test 1"
+		response: "The Designers Republic was founded in 1986 by Ian Anderson, a self-taught designer who got his start by creating flyers for electronic music shows that he organized. The studio dissolved in 2009."
 	},
 
 //***Q2***
 	{
-		questionText: "Test Q2",
+		questionText: "A prolific British graphic designer and author of “The Art of Looking Sideways,” he designed this logo for the Victoria & Albert museum in 1990, which is still in use today.",
 
 		answers: 
 		[ 
 			{
-				text: "test1",
+				text: "Massimo Vignelli",
+
+				correct: false
+			},
+
+			{
+				text: "Paul Rand",
+
+				correct: false
+			},
+
+			{
+				text: "Saul Bass",
+
+				correct: false
+			},
+
+			{
+				text: "Alan Fletcher",
 
 				correct: true
-			},
-
-			{
-				text: "test2",
-
-				correct: false
-			},
-
-			{
-				text: "test3",
-
-				correct: false
-			},
-
-			{
-				text: "test4",
-
-				correct: false
 			}
 		],
 
-		response: "Response Test 2"
+		response: "Alan Fletcher is recognized as one of the most influential figures in post-war British design. His book \"The Art of Looking Sideways\" is over one-thousand pages in length and is meant to be opened at random."
 	},
 
 //***Q3***
 	{
-		questionText: "Test Q3",
+		questionText: "Music is where this designer got his start. He is credited for iconic record covers he produced as the director of Factory Records, including those for Joy Division and New Order.",
 
 		answers: 
 		[ 
 			{
-				text: "test1",
+				text: "Chip Kidd",
+
+				correct: false
+			},
+
+			{
+				text: "Stefan Sagmeister",
+
+				correct: false
+			},
+
+			{
+				text: "Peter Saville",
 
 				correct: true
 			},
 
 			{
-				text: "test2",
-
-				correct: false
-			},
-
-			{
-				text: "test3",
-
-				correct: false
-			},
-
-			{
-				text: "test4",
+				text: "Milton Glaser",
 
 				correct: false
 			}
 		],
 
-		response: "Response Test 3"
+		response: "Peter Saville has multiple covers included in the permanent collection of New Yorks Museum of Modern Art. The colors blocks included on New Order’s Power Corruption and Lies were selected using an elaborate color-based code that spells the band’s name and album title."
 	},
 
 //***Q4***
 	{
-		questionText: "Test Q4",
+		questionText: "Famous for purportedly creating Citi bank’s logo on the back of a napkin in a matter of seconds, this designer also a notable painter of city maps.",
 
 		answers: 
 		[ 
 			{
-				text: "test1",
+				text: "Paula Scher",
 
 				correct: true
 			},
 
 			{
-				text: "test2",
+				text: "Michael Bierut",
 
 				correct: false
 			},
 
 			{
-				text: "test3",
+				text: "Bruce Mau",
 
 				correct: false
 			},
 
 			{
-				text: "test4",
+				text: "Jessica Walsh",
 
 				correct: false
 			}
 		],
 
-		response: "Response Test 4"
+		response: "Paul Scher has also created some of the most recognizable logotypes including work for the Museum of Modern Art and Microsoft."
 	},
 
 //***Q5***
 	{
-		questionText: "Test Q5",
+		questionText: "A professional surfer prior to entering graphic design, his work for Ray Gun magazine is credited for popularizing his iconic grunge typography.",
 
 		answers: 
 		[ 
 			{
-				text: "test1",
+				text: "Susan Kare",
+
+				correct: false
+			},
+
+			{
+				text: "David Carson",
 
 				correct: true
 			},
 
 			{
-				text: "test2",
+				text: "April Greiman",
 
 				correct: false
 			},
 
 			{
-				text: "test3",
-
-				correct: false
-			},
-
-			{
-				text: "test4",
+				text: "John Maeda",
 
 				correct: false
 			}
 		],
 
-		response: "Response Test 5"
+		response: "David Carson was the 9th best surfer in America at his peak. After his work as art director of Transworld Skateboarding, he was hired by Ray Gun in 1992 and founded his own studio in 1995."
 	}
 ];
 
@@ -267,29 +267,37 @@ function evaluate() {
 
 	//Shows response and waits for setTimeout to print next question.
 	var nextQuestion = setTimeout(function() {
-		if (questionNum < 5) {
+		if (questionNum < triviaBank.length) {
 			// var nextQuestion = setTimeout(function () {
 				printQuestion();
 			};
 
 		//Results Page.
-		if (questionNum === 5) {
+		if (questionNum === triviaBank.length) {
 			$('.triviaBox').empty()
-			.text('Game Over');
+			.text('Thanks for playing! You answered ' + correctGuess + ' questions correctly and got ' + wrongGuess + ' wrong. Do you want to try again?');
+			$('.resetBtn').removeClass('hidden');
 		}
 	}, 8000);
 };
 
-//Restart values on restart
-function restart() {
-	var questionNum = 0;
+//Reset values on restart
+$('.resetBtn').on('click', function() {
+	questionNum = 0;
 
-	var currentQuestion = triviaBank[0];
+	currentQuestion = triviaBank[0];
 
-	var correctGuess = 0;
+	correctGuess = 0;
 
-	var wrongGuess = 0;
-};
+	wrongGuess = 0;
+
+	$(this).addClass('hidden');
+
+	printQuestion();
+});
 
 //Starts game.
-printQuestion();
+$('.startBtn').on('click', function () {
+	printQuestion();
+	$(this).addClass('hidden');
+});
